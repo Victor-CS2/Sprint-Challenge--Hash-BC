3e3220394674e42335c43c96d1ce714af590b8b5
victorN <victornguyen082@gmail.com>
2019-12-06 11:22:48 -0500

    example one complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,12 +6,20 @@ from hashtables import (HashTable,
                         hash_table_resize)
 
 
-def get_indices_of_item_weights(weights, length, limit):
+def get_indices_of_item_weights(weights, length, limit): # Find two items whose sum of weights equals the weight limit (limit).
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length): # insert weights with the index onto hashtable -- insert
+        hash_table_insert(ht, weights[i], i)
+    # subtract weights from hashtable limit
+    for i in range(length):
+        value = hash_table_retrieve(ht, limit - weights[i]) # limit - index weights
+        # if the value isn't none...
+        if value is not None:
+            # return key, value in acsending order
+            if i > value: # higher valued index should be placed in the "zeroth" index and the smaller index should be placed in the "first" index
+                return (i, value)
+            else: # if the index isn't larger then return value first.
+                return (value, i)
 
     return None
 
